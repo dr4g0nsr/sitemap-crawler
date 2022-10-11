2643d326f60838759f6d4c3264ec961c15cd922b
Cirko <dragonmen@gmail.com>
2022-10-11 19:22:38 +0200

    Correct test

--- test/basic-test.php
+++ test/basic-test.php
@@ -6,14 +6,10 @@ require __DIR__ . '/config.php';
 
 use dr4g0nsr\SitemapCrawler;
 
-//$url = 'https://bonneli-imago-011.co.rs';
 $url = 'https://candymapper.com';
 print "Crawler version: " . SitemapCrawler::version() . PHP_EOL;
 
 $crawler = new SitemapCrawler(['sleep' => 5]);
 $crawler->loadConfig(__DIR__ . '/config.php');
-var_dump($crawler->getSettings());
-die("x");
-
 $sitemap = $crawler->getSitemap($url);
 $crawler->crawlURLS($sitemap);
