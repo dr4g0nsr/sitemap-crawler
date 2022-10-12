260a96ae7c993bc788c7f71686d8253aa170e6ed
Cirko <dragonmen@gmail.com>
2022-10-12 22:13:23 +0200

    Updated test

--- test/basic-test.php
+++ test/basic-test.php
@@ -4,16 +4,11 @@ require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/config.php';
 
 use dr4g0nsr\SitemapCrawler;
-use \GuzzleHttp as http;
-
-$httpClient = new \GuzzleHttp\Client();
 
 $url = 'https://candymapper.com';
-//$url = 'https://godev.link';
 print "Crawler version: " . SitemapCrawler::version() . PHP_EOL;
 
 $crawler = new SitemapCrawler(['sleep' => 5]);
 $crawler->loadConfig(__DIR__ . '/config.php');
 $sitemap = $crawler->getSitemap($url);
-//$crawler->crawlURLS($sitemap);
-var_dump($sitemap);
+$crawler->crawlURLS($sitemap);
