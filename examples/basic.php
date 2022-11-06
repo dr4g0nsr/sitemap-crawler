eb40a9248ec5a9e3cc5454634b0b337ab95caa6d
Cirko <dragonmen@gmail.com>
2022-11-06 22:31:24 +0100

    Tests and refactor

---
+++ examples/basic.php
@@ -0,0 +1,16 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/config.php';
+
+use dr4g0nsr\Crawler;
+
+$url = 'https://candymapper.com';
+print "Crawler version: " . Crawler::version() . PHP_EOL;
+
+$crawler = new Crawler(['sleep' => 0, 'verbose' => true]);
+$crawler->loadConfig(__DIR__ . '/config.php');
+$sitemap = $crawler->getSitemap($url);
+$crawler->crawlURLS($sitemap);
+$stats = $crawler->getStats();
+var_dump($stats);
